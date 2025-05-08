087e59fec1633f3e06d16466b79c0ad3da62532e
Emmanuel <emmanuelnv01@gmail.com>
2025-05-08 15:24:37 +0100

    Refactor .gitignore and add additional ignore rule for 'lib/' directory

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import { Colors } from "../../constants/Colors";
 import { useColorScheme } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const DashboardLayout = () => {
   const colorScheme = useColorScheme();
@@ -18,7 +19,53 @@ const DashboardLayout = () => {
         tabBarActiveTintColor: theme.iconColorFocused,
         tabBarInactiveTintColor: theme.iconColor,
       }}
-    />
+    >
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                size={24}
+                name={focused ? "person-circle" : "person-circle-outline"}
+                color={focused ? theme.iconColorFocused : theme.iconColor}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="fightSessions"
+        options={{
+          title: "Sessions",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                size={24}
+                name={focused ? "barbell" : "barbell-outline"}
+                color={focused ? theme.iconColorFocused : theme.iconColor}
+              />
+            );
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Ionicons
+                size={24}
+                name={focused ? "add-circle" : "add-circle-outline"}
+                color={focused ? theme.iconColorFocused : theme.iconColor}
+              />
+            );
+          },
+        }}
+      />
+    </Tabs>
   );
 };
 
--- components/ThemedView.jsx
+++ components/ThemedView.jsx
@@ -2,14 +2,39 @@ import { View } from "react-native";
 import React from "react";
 import { Colors } from "../constants/Colors";
 import { useColorScheme } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const ThemedView = ({ style, ...props }) => {
+const ThemedView = ({ style, safe = false, ...props }) => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
 
+  if (!safe)
+    return (
+      <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    );
+
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={[{ backgroundColor: theme.background }, style]} {...props} />
+    <View
+      style={[
+        {
+          backgroundColor: theme.background,
+          paddingTop: insets.top,
+          paddingBottom: insets.bottom,
+        },
+        style,
+      ]}
+      {...props}
+    />
   );
 };
 
 export default ThemedView;
+
+import { Client, Account } from "react-native-appwrite";
+
+const client = new Client()
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject("681cbcf400386489d810")
+  .setPlatform("dev.manny.learnReact");
